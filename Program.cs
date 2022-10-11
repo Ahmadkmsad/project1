56473c289fc23c4c08c9ac94b402d0f3e26dcb4a
Ahmadkmsad <100020294+Ahmadkmsad@users.noreply.github.com>
2022-10-11 06:21:11 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -161,4 +161,150 @@ class Program
         Console.Read();
     }
 }
+///////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{   class Candidate
+    {
+        public string name;
+        public string cnic;
+        public string city;
+        public double gpa;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Candidate Candidate1 = new Candidate();
+           Candidate1.name="Ahmad Shaukat";
+           Candidate1.cnic = "3520227628485";
+           Candidate1.city = "Lahore";
+           Candidate1.gpa = 3.315;
+
+           Candidate Candidate2 = new Candidate();
+           Candidate2.name = "Faraz Abbas";
+           Candidate2.cnic = "3522937628485";
+           Candidate2.city = "Lahore";
+           Candidate2.gpa = 3.8;
+
+           Candidate Candidate3 = new Candidate();
+           Candidate3.name = "Jamshaid Akhtar";
+           Candidate3.cnic = "3522937628485";
+           Candidate3.city = "Lahore";
+           Candidate3.gpa = 2.7;
+
+           Candidate Candidate4 = new Candidate();
+           Candidate4.name = "Noman Khan";
+           Candidate4.cnic = "3522937628485";
+           Candidate4.city = "Lahore";
+           Candidate4.gpa = 2.9;
+
+           Candidate Candidate5 = new Candidate();
+           Candidate5.name = "Nawaz sharif";
+           Candidate5.cnic = "3522937628485";
+           Candidate5.city = "Lahore";
+           Candidate5.gpa = 3.3;
+
+           Candidate Candidate6 = new Candidate();
+           Candidate6.name = "Asif zardari";
+           Candidate6.cnic = "3522937628485";
+           Candidate6.city = "Lahore";
+           Candidate6.gpa = 3.5;
+
+           Candidate Candidate7 = new Candidate();
+           Candidate7.name = "Bilawal Bhuto";
+           Candidate7.cnic = "3522937628485";
+           Candidate7.city = "Lahore";
+           Candidate7.gpa = 2.7;
+
+           Candidate Candidate8 = new Candidate();
+           Candidate8.name = "Atif Anjum";
+           Candidate8.cnic = "3522937628485";
+           Candidate8.city = "Lahore";
+           Candidate8.gpa = 3.0;
+
+           Candidate Candidate9 = new Candidate();
+           Candidate9.name = "Faraz Abbas";
+           Candidate9.cnic = "3522937628485";
+           Candidate9.city = "Rawalpindi";
+           Candidate9.gpa = 3.8;
+
+           Candidate Candidate10 = new Candidate();
+           Candidate10.name = "Faraz Abbas";
+           Candidate10.cnic = "3522937628485";
+           Candidate10.city = "Lahore";
+           Candidate10.gpa = 2.4;
+
+           Candidate Candidate11 = new Candidate();
+           Candidate11.name = "Faraz Abbas";
+           Candidate11.cnic = "3522937628485";
+           Candidate11.city = "Rawalpindi";
+           Candidate11.gpa = 3.8;
+
+           Candidate Candidate12 = new Candidate();
+           Candidate12.name = "Khalil Ijaz";
+           Candidate12.cnic = "3522937628485";
+           Candidate12.city = "Lahore";
+           Candidate12.gpa = 3.8;
+
+           Candidate Candidate13 = new Candidate();
+           Candidate13.name = "Faraz Abbas";
+           Candidate13.cnic = "3522937628485";
+           Candidate13.city = "Rawalpindi";
+           Candidate13.gpa = 3.8;
+
+           Candidate Candidate14 = new Candidate();
+           Candidate14.name = "Faraz Abbas";
+           Candidate14.cnic = "3522937628485";
+           Candidate14.city = "Lahore";
+           Candidate14.gpa = 3.8;
+
+           Candidate Candidate15 = new Candidate();
+           Candidate15.name = "Faraz Abbas";
+           Candidate15.cnic = "3522937628485";
+           Candidate15.city = "Rawalpindi";
+           Candidate15.gpa = 2.4;
+
+           List<Candidate> Customers = new List<Candidate>();
+           List<Candidate> filterList= new List<Candidate>();
+            
+           Customers.Add(Candidate1);
+           Customers.Add(Candidate2);
+           Customers.Add(Candidate3);
+           Customers.Add(Candidate4);
+           Customers.Add(Candidate5);
+           Customers.Add(Candidate6);
+           Customers.Add(Candidate7);
+           Customers.Add(Candidate8);
+           Customers.Add(Candidate9);
+           Customers.Add(Candidate10);
+           Customers.Add(Candidate11);
+           Customers.Add(Candidate12);
+           Customers.Add(Candidate13);
+           Customers.Add(Candidate14);
+           Customers.Add(Candidate15);
+
+
+           for (int i = 0; i < 15;i++ )
+           {
+              if (Customers[i].gpa>=3)
+              {
+                  filterList.Add(Customers[i]);
+              }  
+               
+           }
+           
+               
+           
+            
+            Console.Read();
+        }
+    }
+}
 
